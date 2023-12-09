b76035f8ed6a721220fb13db21e20cc3588b519e
16Veronika <14.siana.11@gmail.com>
2023-12-09 21:44:50 +0200

    Homework_3

---
+++ work3/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задайте массив из вещественных чисел с
+// ненулевой дробной частью. Найдите разницу между
+// максимальным и минимальным элементов массива. 
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int arraySize = 10;
+
+        double[] numbers = new double[arraySize];
+
+        for (int i = 0; i < arraySize; i++)
+        {
+            numbers[i] = new Random().NextDouble() * 10; 
+        }
+
+        Console.Write("Массив вещественных чисел: [");
+        for (int i = 0; i < arraySize; i++)
+        {
+            Console.Write($" {numbers[i]:F2} ");
+        }
+
+        double maxNumber = numbers[0];
+        double minNumber = numbers[0];
+
+        for (int i = 1; i < arraySize; i++)
+        {
+            if (numbers[i] > maxNumber)
+            {
+                maxNumber = numbers[i];
+            }
+
+            if (numbers[i] < minNumber)
+            {
+                minNumber = numbers[i];
+            }
+        }
+
+        double difference = maxNumber - minNumber;
+
+        Console.WriteLine($" ] => {difference:F2}");
+    }
+}
