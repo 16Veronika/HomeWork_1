2cb38f6367fef6f25c79167fead1b863f8d18819
16Veronika <14.siana.11@gmail.com>
2023-12-09 21:09:05 +0200

    Homework_2

---
+++ work2/Program.cs
@@ -0,0 +1,34 @@
+﻿//Задайте массив на 10 целых чисел.
+//Напишите программу, которая определяет
+//количество чётных чисел в массиве
+
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Создаем массив из 5 целых чисел
+        int[] array = new int[10]; 
+        for (int i = 0; i < array.Length; i++)
+       { 
+        array[i] = new Random().Next(0,100);
+        
+       }
+        // Находим количество элементов массива, значения которых лежат в отрезке [20, 90]
+        int count = 0;
+        for (int i = 0; i < array.Length; i++)
+       
+        {
+            int element = array[i];
+            if (element %2 == 0)
+            {
+                count++;
+            }
+        }
+
+        // Выводим результат в указанном формате
+        Console.WriteLine($" массив [{string.Join(" ", array)}] => {count}");
+    }
+}
